24ef1d67d827bd4c42610301e43ac412dfc6982b
xx18 <xx18@global.tesco.org>
2017-12-29 01:17:03 +0530

    Added Main component

---
+++ src/app/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <div className="conatiner">
+                <div className="jumbotron text-center">
+                    <h3>Admissions open for 2018!!!</h3>
+                    <p>Admission opens for Prajwal Academy School rush to book seats for your kids.Prajwal Academy School is a school of full values, 
+                        ethics, equality and many more. Willing to bring up your kids with presented values then contact us. </p>
+                </div>
+            </div>
+        )
+    }
+}
